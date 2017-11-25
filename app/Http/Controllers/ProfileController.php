77082c0270ef46e9ede07c4f3d7fe0e30ca8ee86
thananji19 <stjeyan@gmail.com>
2017-11-26 10:34:05 +1100

    Carers Couch production

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    //
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function show()
+    {
+        return view('profile.show');
+    }
+
+    public function edit()
+    {
+        return view('profile.edit');
+    }
+
+    public function update()
+    {
+        return redirect('profile');
+        //return view('home');
+    }
+
+}
